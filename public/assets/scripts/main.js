9628229dd5c1a650bcc9a102c6dab9922d4e0a36
jamesm1984 <artur@Jamess-MacBook-Pro.local>
2014-11-17 19:13:47 -0500

    Update: Functionality
    fixed multiples of the same post.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -9,38 +9,32 @@ jQuery(function($) {
     var socket = io();
 
     socket.on('connected',function(id){
-      // // updates user list
-      // socket.on('userUpdate', function(user_connected){
-      //   var message = "<li id='userID-" + user_connected + "'>" + user_connected + "</li>";
-      //   $('#users_list').append(message);
-      // });
-      console.log(socket.users);
+
       var message = "<li id='userID-" + id + "'>userID-" + id + "</li>";
       $('#users_list').append(message);
 
       if(firstConnection){
         userID=id;
         firstConnection=false;
-        // // sends an update to the server when a user connects
-        // socket.emit('userConnected', userID);
       }
 
 
-
-      // Send A Message To The Server
-      $('#chat_submit').on('click', function(){
-        var text = $('#chat_message').html();
-        socket.emit('message', text, userID);
-      });
     });
-// Recieve Update Event From The Server
-      socket.on('update', function(msg, id){
-        var message = "<span class='user_name'> User-" + id + "</span> <p>" + msg + "</p>";
-        var chat = $('#chat_window').html();
-        $('#chat_window').html("");
 
-        $('#chat_window').html(chat+message+"<br>");
+    // Send A Message To The Server
+    $('#chat_submit').on('click', function(){
+      var text = $('#chat_message').html();
+      socket.emit('message', text, userID);
+    });
 
-      });
+    // Recieve Update Event From The Server
+    socket.on('update', function(msg, id){
+      var message = "<span class='user_name'> User-" + id + "</span> <p>" + msg + "</p>";
+      var chat = $('#chat_window').html();
+      $('#chat_window').html("");
+
+      $('#chat_window').html(chat+message+"<br>");
+
+    });
 
 });
