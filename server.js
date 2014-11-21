979c4d83987b36a55aec1318c088b10422561940
jamesm1984 <artur@Jamess-MacBook-Pro.local>
2014-11-20 20:49:16 -0500

    Update: Functionality
    updated css, added disconnect function, added name change chat alert

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
 //////////////////////////////////
 // Require Native Node.js Libraries
 //////////////////////////////////
-
 var express = require('express');
 var app = express();
 var http = require('http');
@@ -10,38 +8,37 @@ http = http.Server(app);
 var io = require('socket.io');
 io = io(http);
 var users=[];
-var messages;
 var id=0;
 //////////////////////////////////
 // Route our Assets
 //////////////////////////////////
-
 app.use('/assets/', express.static(__dirname + '/public/assets/'));
-
 //////////////////////////////////
 // Route our Home Page
 //////////////////////////////////
-
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
-
 //////////////////////////////////
 // Handle Socket Connection
 //////////////////////////////////
-
 io.on('connection', function(socket){
-
   console.log('A User Connected' + id);
 
-  //add user to users array
-  users.push({id:id, name:null});
-  console.log(users);
+  // store
+  socket.uid = id;
 
+  //add user to users array
+  users.push({
+    id:id,
+    name:null,
+    status:"connected"
+  });
+
+  console.log(users);
 
   //emit to the client their id
   io.emit('connected',id, users);
-
 
   // Handle Message Event
   socket.on('message', function(text, userID){
@@ -51,18 +48,26 @@ io.on('connection', function(socket){
   // Handle Message Event
   socket.on('username', function(name, userID){
     users[userID].name = name;
-    io.emit('update_username', id, users);
+    io.emit('update_username', userID, users);
+  });
+
+  // Disconnect user
+  socket.on('disconnect', function() {
+
+    console.log(socket.uid);
+    users[socket.uid].status = "disconnected";
+    io.emit('update_disconnect', socket.uid, users);
+    console.log("user " + socket.uid + " has disconnected!");
+
   });
 
   //Increment user id
   id = id + 1;
 });
-
 //////////////////////////////////
 // Start Server
 //////////////////////////////////
-
 http.listen(process.env.PORT || 3000, process.env.IP || "127.0.0.1", function(){
-  var addr = http.address();
-  console.log("Server started at", addr.address + ":" + addr.port);
+var addr = http.address();
+console.log("Server started at", addr.address + ":" + addr.port);
 });
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,160 +1,166 @@
-
 // Wait for DOM to Load
 jQuery(function($) {
+	var userID;
+	var firstConnection=true;
 
-    var userID;
-    var firstConnection=true;
+	// Create New Socket Connection using Socket.io
+	var socket = io();
 
-    // Create New Socket Connection using Socket.io
-    var socket = io();
+	socket.on('connected',function(id, users){
+		if(firstConnection){
+			userID=id;
+			firstConnection=false;
+		}
 
-    socket.on('connected',function(id, users){
+		$('#users_list').html('');
 
-      if(firstConnection){
-        userID=id;
-        firstConnection=false;
-      }
+		if (users[userID].name != null){
+			var message = "<li id='userID-" + userID + "'class='user'><p>" + users[userID].name + "</p></li>";
+			$('#users_list').append(message);
+		} else {
+			var message = "<li id='userID-" + userID + "'class='user'><p>userID-" + userID + "</p></li>";
+			$('#users_list').append(message);
+		};
 
-      $('#users_list').html('');
+		for(i=0; i < users.length; i++){
+			if (i!=userID && users[i].status === "connected"){
+				if(users[i].name == null){
+					var message = "<li id='userID-" + i + "'><p>userID-" + i + "</p></li>";
+					$('#users_list').append(message);
+				} else{
+					var message = "<li id='userID-" + i + "'><p>" + users[i].name + "</p></li>";
+					$('#users_list').append(message);
+				}
+			};
+		};
 
-      if (users[userID].name != null){
-        var message = "<li id='userID-" + id + "'class='user'><p>" + users[userID].name + "</p></li>";
-        $('#users_list').append(message);
-      } else {
-        var message = "<li id='userID-" + id + "'class='user'><p>userID-" + id + "</p></li>";
-        $('#users_list').append(message);
-      }
+		$('.user').on('click', function(){
+			var name;
+			var results=[];
+			var filter;
+			name = window.prompt("Enter your custom username must be less then 10 characters", "Username");
 
-      for(i=0; i < users.length; i++){
-        if (i!=userID){
-          if(users[i].name == null){
-            var message = "<li id='userID-" + i + "'><p>userID-" + i + "</p></li>";
-            $('#users_list').append(message);
-          } else{
-            var message = "<li id='userID-" + i + "'><p>" + users[i].name + "</p></li>";
-            $('#users_list').append(message);
-          }
-        };
-      };
+			function checkForDuplicates(){
+				for (i=0; i<users.length; i++){
+					if(users[i].name != name && users[i].status === "connected"){
+						results.push(false);
+					} else if(users[i].name === name && users[i].status === "connected"){
+						results.push(true);
+					}
+				}
+				filter = results.indexOf(true);
+				console.log(name.length);
+				if(filter >=0 && name.length>10 || filter >=0 || name.length>10 || name == ""){
+					name = window.prompt("It appears someone already has that username or username was over 10 characters please try again", "Username");
+					results=[];
+					checkForDuplicates()
+				}else{
+					socket.emit('username', name, userID);
+				}
+			}
+			checkForDuplicates()
+		});
+	});
 
-      $('.user').on('click', function(){
+	// Send A Message To The Server
+	$('#chat_submit').on('click', function(){
+		var text = $('#chat_message').html();
+		socket.emit('message', text, userID);
+	});
 
-        var name;
-        var results=[];
-        var filter;
+	// Recieve Update Event From The Server
+	socket.on('update', function(text, id, users){
+		var chat = $('#chat_window').html();
+		if(users[id].name!=null){
+			var message = "<div class='message'><span class='user_name'>" + users[id].name + ":</span><div class='content'>" + text + "</div></div><hr>";
+		} else {
+			var message = "<div class='message'><span class='user_name'> User-" + id + ":</span><div class='content'>" + text + "</div></div><hr>";
+		}
 
-        name = window.prompt("Enter your custom username must be less then 10 characters", "Username");
+		$('#chat_window').html("");
+		$('#chat_window').html(chat+message);
+	});
+	socket.on('update_disconnect',function(id, users){
+		$('#users_list').html('');
 
-        function checkForDuplicates(){
-          for (i=0; i<users.length; i++){
+		if (users[userID].name != null){
+			var message = "<li id='userID-" + userID + "'class='user'><p>" + users[userID].name + "</p></li>";
+			$('#users_list').append(message);
+		} else {
+			var message = "<li id='userID-" + userID + "'class='user'><p>userID-" + userID + "</p></li>";
+			$('#users_list').append(message);
+		}
+		for(i=0; i < users.length; i++){
+			if (i!=userID && users[i].status === "connected"){
+				if(users[i].name == null){
+					var message = "<li id='userID-" + i + "'><p>userID-" + i + "</p></li>";
+					$('#users_list').append(message);
+				} else{
+					var message = "<li id='userID-" + i + "'><p>" + users[i].name + "</p></li>";
+					$('#users_list').append(message);
+				}
+			};
+		};
 
-            if(users[i].name != name){
-              results.push(false);
-            } else{
-              results.push(true);
-            }
+		if(users[id].name === null){
+			$('#chat_window').append("<p class='disconnected'>userID-" + id + " has disconnected</p>");
+		} else{
+			$('#chat_window').append("<p class='disconnected'>" + users[id].name + " has disconnected</p>");
+		}
 
-          }
+	});
+	socket.on('update_username',function(id, users){
+		$('#chat_window').append("<p class='changedNames'>userID-" + id + " has changed their name to " + users[id].name + "</p>");
+		$('#users_list').html('');
 
-          filter = results.indexOf(true);
-          console.log(name.length);
-          if(filter >=0 && name.length>10 || filter >=0 || name.length>10 || name == ""){
-            name = window.prompt("It appears someone already has that username or username was over 10 characters please try again", "Username");
-            results=[];
-            checkForDuplicates()
-          }else{
-            socket.emit('username', name, userID);
-          }
-        }
+		if (users[userID].name != null){
+			var message = "<li id='userID-" + userID + "'class='user'><p>" + users[userID].name + "</p></li>";
+			$('#users_list').append(message);
+		} else {
+			var message = "<li id='userID-" + userID + "'class='user'><p>userID-" + userID + "</p></li>";
+			$('#users_list').append(message);
+		}
+		for(i=0; i < users.length; i++){
+			if (i!=userID && users[i].status === "connected"){
+				if(users[i].name == null){
+					var message = "<li id='userID-" + i + "'><p>userID-" + i + "</p></li>";
+					$('#users_list').append(message);
+				} else{
+					var message = "<li id='userID-" + i + "'><p>" + users[i].name + "</p></li>";
+					$('#users_list').append(message);
+				}
+			};
+		};
+		$('.user').on('click', function(){
+			var name;
+			var results=[];
+			var filter;
 
-        checkForDuplicates()
+			name = window.prompt("Enter your custom username must be less then 10 characters", "Username");
 
-      });
+			function checkForDuplicates(){
 
-    });
+				for (i=0; i<users.length; i++){
+					if(users[i].name != name && users[i].status === "connected"){
+						results.push(false);
+					} else if(users[i].name === name && users[i].status === "connected"){
+						results.push(true);
+					}
+				}
 
-    // Send A Message To The Server
-    $('#chat_submit').on('click', function(){
-      var text = $('#chat_message').html();
-      socket.emit('message', text, userID);
-    });
+				filter = results.indexOf(true);
+				console.log(name.length);
 
+				if(filter >=0 && name.length>10 || filter >=0 || name.length>10 || name == ""){
+					name = window.prompt("It appears someone already has that username or username was over 10 characters please try again", "Username");
+					results=[];
+					checkForDuplicates()
+				}else{
+					socket.emit('username', name, userID);
+				}
+			}
+			checkForDuplicates()
+		});
 
-
-    // Recieve Update Event From The Server
-    socket.on('update', function(text, id, users){
-      if(users[id].name!=null){
-        var message = "<span class='user_name'>" + users[id].name + "</span> <p>" + text + "</p><hr>";
-      } else {
-        var message = "<span class='user_name'> User-" + id + "</span> <p>" + text + "</p><hr>";
-      }
-
-
-      var chat = $('#chat_window').html();
-      $('#chat_window').html("");
-
-      $('#chat_window').html(chat+message+"<br>");
-
-    });
-
-    socket.on('update_username',function(id, users){
-      console.log(users);
-      $('#users_list').html('');
-
-      if (users[userID].name != null){
-        var message = "<li id='userID-" + id + "'class='user'><p>" + users[userID].name + "</p></li>";
-        $('#users_list').append(message);
-      } else {
-        var message = "<li id='userID-" + id + "'class='user'><p>userID-" + id + "</p></li>";
-        $('#users_list').append(message);
-      }
-
-
-      for(i=0; i < users.length; i++){
-        if (i!=userID){
-          if(users[i].name == null){
-            var message = "<li id='userID-" + i + "'><p>userID-" + i + "</p></li>";
-            $('#users_list').append(message);
-          } else{
-            var message = "<li id='userID-" + i + "'><p>" + users[i].name + "</p></li>";
-            $('#users_list').append(message);
-          }
-        };
-
-      };
-      $('.user').on('click', function(){
-
-        var name;
-        var results=[];
-        var filter;
-
-        name = window.prompt("Enter your custom username must be less then 10 characters", "Username");
-
-        function checkForDuplicates(){
-          for (i=0; i<users.length; i++){
-
-            if(users[i].name != name){
-              results.push(false);
-            } else{
-              results.push(true);
-            }
-
-          }
-
-          filter = results.indexOf(true);
-          console.log(name.length);
-          if(filter >=0 && name.length>10 || filter >=0 || name.length>10 || name == ""){
-            name = window.prompt("It appears someone already has that username or username was over 10 characters please try again", "Username");
-            results=[];
-            checkForDuplicates()
-          }else{
-            socket.emit('username', name, userID);
-          }
-        }
-
-        checkForDuplicates()
-
-      });
-    });
-
+	});
 });
